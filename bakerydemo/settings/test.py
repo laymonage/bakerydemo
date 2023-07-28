2c4e399dcf88daa2cfd1f35692f2e628ce64c90e
Sage Abdullah <sage.abdullah@torchbox.com>
2023-07-28 12:01:55 +0100

    Run tests/benchmark server with debug turned off

--- bakerydemo/settings/test.py
+++ bakerydemo/settings/test.py
@@ -2,3 +2,10 @@ from bakerydemo.settings.dev import *  # noqa
 
 # Always enable Django Debug Toolbar.
 DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda r: True}
+
+# Disable debug mode to simulate production environment.
+DEBUG = False
+
+# Use whitenoise to serve static files.
+MIDDLEWARE.append("whitenoise.middleware.WhiteNoiseMiddleware")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
