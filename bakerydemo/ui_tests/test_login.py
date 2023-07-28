61972bd298ace5cbbf2a702677bcabc5f247bab1
Sage Abdullah <sage.abdullah@torchbox.com>
2023-07-28 10:15:31 +0100

    Revert using live_server fixture and run test server separately instead
    The live_server fixture runs in a separate thread and makes it hard to
    load the bakerydemo fixtures. It's also much slower when we start adding
    more tests, because the live server needs to be set up for each test
    (which includes running migrations etc.).
    
    We don't really need transactional tests anyway. We would like to
    emulate a real server as much as possible, which would mean having
    complex scenarios that need to be executed in order (e.g. going through
    workflows). It would be handy to keep the server's database state
    across tests, even though it means that the tests might not be isolated.

--- bakerydemo/ui_tests/test_login.py
+++ bakerydemo/ui_tests/test_login.py
@@ -1,17 +1,12 @@
 import re
 
-from django.core.management import call_command
 from playwright.sync_api import Page, expect
-from pytest import fixture
+
+LIVE_SERVER_URL = "http://localhost:8000"
 
 
-@fixture
-def data():
-    call_command("load_initial_data")
-
-
-def test_login_wagtail_admin(page: Page, live_server, data):
-    page.goto(f"{live_server.url}/admin")
+def test_login_wagtail_admin(page: Page):
+    page.goto(f"{LIVE_SERVER_URL}/admin")
 
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Sign in - Wagtail"))
@@ -27,6 +22,6 @@ def test_login_wagtail_admin(page: Page, live_server, data):
     sign_in_button.click()
 
     # Expects the URL to contain /admin/.
-    expect(page).to_have_url(re.compile(f"^{live_server.url}/admin/$"))
+    expect(page).to_have_url(re.compile(f"^{LIVE_SERVER_URL}/admin/$"))
     expect(page).to_have_title(re.compile("Dashboard - Wagtail"))
     expect(page.get_by_text("Welcome to the bakerydemo Wagtail CMS")).to_be_visible()
